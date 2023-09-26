27cd42005189dbbc340a5914d3a67db953f5aec8
Hilliam Tung <hilliamtung@gmail.com>
2023-09-26 17:32:20 +0100

    Added simple aori_ping example bot

---
+++ 00-aori-ping/index.ts
@@ -0,0 +1,20 @@
+import { XYK } from "@aori-io/sdk";
+import "dotenv/config";
+import { AnkrProvider, Wallet } from "ethers";
+import { WebSocket } from "ws";
+
+const ws = new WebSocket(process.env.BOTS_WEBSOCKET_URL || "ws://localhost:8080");
+const subscriptions = new WebSocket(process.env.BOTS_SUBSCRIPTION_URL || "ws://localhost:8081");
+const wallet = new Wallet(process.env.BOT_PRIVATE_KEY || "0x0000000000000000000000000000000000000000000000000000000000000000")
+const provider = new AnkrProvider(process.env.CHAIN || "goerli", process.env.ANKR_API_KEY);
+
+ws.on("open", async () => {
+    const xyk = new XYK(
+        ws,
+        subscriptions,
+        wallet,
+        provider
+    );
+
+    await xyk.ping();
+});
