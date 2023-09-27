f0db16c20dcfdd9169d4296267d736c7ca97de4f
Hilliam Tung <hilliamtung@gmail.com>
2023-09-27 10:03:48 +0100

    Added a simple 1-for-1 bot

---
+++ 01-one-for-one/OneForOne.ts
@@ -0,0 +1,40 @@
+import { defaultOrderAddress, LimitOrderManager, OrderToExecute, Order__factory, ResponseEvents } from "@aori-io/sdk";
+import { Signature } from "ethers";
+
+export class OneForOneBot extends LimitOrderManager {
+
+    aTokenAddress: string = "";
+    bTokenAddress: string = "";
+    chainId: number = 5;
+
+    async initialise({ chainId, aTokenAddress, bTokenAddress }: { chainId: number, aTokenAddress: string, bTokenAddress: string }): Promise<void> {
+        super.initialise();
+
+        this.chainId = chainId;
+        this.aTokenAddress = aTokenAddress;
+        this.bTokenAddress = bTokenAddress;
+
+        this.on(ResponseEvents.SubscriptionEvents.OrderTaken, async (orderHash) => {
+            await this.refreshOrder();
+        });
+
+        this.on(ResponseEvents.NotificationEvents.OrderToExecute, async ({ parameters, signature }: OrderToExecute) => {
+            const order = Order__factory.connect(defaultOrderAddress, this.provider);
+            await order.settleOrders(parameters, Signature.from(signature));
+        });
+
+        this.refreshOrder();
+    }
+
+    async refreshOrder(): Promise<void> {
+        const limitOrder = await this.createLimitOrder({
+            inputToken: this.aTokenAddress,
+            outputToken: this.bTokenAddress,
+            inputAmount: 10n ** 18n,
+            outputAmount: 10n ** 18n,
+            chainId: this.chainId
+        });
+
+        await this.makeOrder({ order: limitOrder, chainId: this.chainId });
+    }
+}
