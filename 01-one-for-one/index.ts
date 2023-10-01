c124194adba11147bfad8aa0443c7fc9b8906a30
Hilliam Tung <hilliamtung@gmail.com>
2023-10-01 01:30:06 +0100

    updated sdk to 0.1.4; simplified bot creation

--- 01-one-for-one/index.ts
+++ 01-one-for-one/index.ts
@@ -1,28 +1,19 @@
 import "dotenv/config";
 import { AnkrProvider, Wallet } from "ethers";
-import { WebSocket } from "ws";
 import { OneForOneBot } from "./OneForOne";
 
-const ws = new WebSocket(process.env.BOTS_WEBSOCKET_URL || "ws://localhost:8080");
-const subscriptions = new WebSocket(process.env.BOTS_SUBSCRIPTION_URL || "ws://localhost:8081");
 const wallet = new Wallet(process.env.BOT_PRIVATE_KEY || "0x0000000000000000000000000000000000000000000000000000000000000000")
 const provider = new AnkrProvider(process.env.CHAIN || "goerli", process.env.ANKR_API_KEY);
 
-ws.on("open", async () => {
-    const bot = new OneForOneBot(
-        ws,
-        subscriptions,
-        wallet,
-        provider
-    );
+const bot = new OneForOneBot({
+    wallet,
+    provider
+});
 
+bot.on("ready", async () => {
     await bot.initialise({
         aTokenAddress: process.env.A_TOKEN_ADDRESS || "",
         bTokenAddress: process.env.B_TOKEN_ADDRESS || "",
         chainId: 5
     });
-});
-
-ws.on("error", (e) => {
-    console.log(e);
 });
--- 02-constant-market-maker/index.ts
+++ 02-constant-market-maker/index.ts
@@ -1,21 +1,16 @@
 import "dotenv/config";
 import { AnkrProvider, Wallet } from "ethers";
-import { WebSocket } from "ws";
 import { ConstantMarketMaker } from "./CMM";
 
-const ws = new WebSocket(process.env.BOTS_WEBSOCKET_URL || "ws://localhost:8080");
-const subscriptions = new WebSocket(process.env.BOTS_SUBSCRIPTION_URL || "ws://localhost:8081");
 const wallet = new Wallet(process.env.BOT_PRIVATE_KEY || "0x0000000000000000000000000000000000000000000000000000000000000000")
 const provider = new AnkrProvider(process.env.CHAIN || "goerli", process.env.ANKR_API_KEY);
 
-ws.on("open", async () => {
-    const bot = new ConstantMarketMaker(
-        ws,
-        subscriptions,
-        wallet,
-        provider
-    );
+const bot = new ConstantMarketMaker({
+    wallet,
+    provider
+});
 
+bot.on("ready", async () => {
     await bot.initialise({
         aTokenAddress: process.env.A_TOKEN_ADDRESS || "",
         bTokenAddress: process.env.B_TOKEN_ADDRESS || "",
@@ -23,8 +18,4 @@ ws.on("open", async () => {
         bAmount: 2n * 10n ** 18n,
         chainId: 5
     });
-});
-
-ws.on("error", (e) => {
-    console.log(e);
 });
